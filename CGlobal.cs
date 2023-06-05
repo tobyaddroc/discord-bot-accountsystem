090638b4778ae350fb63701026d9901fec8f6d72
unknown <tobyaddroc>
2023-06-05 23:35:44 +0400

    suka token zabil ubrat

--- CGlobal.cs
+++ CGlobal.cs
@@ -9,8 +9,8 @@ namespace DiscordBotAccountSystem
         public static FileStream GroupsHandle;
         public static class CConfig
         {
-            public static readonly string Token = "MTA1OTkwNTc3NzI1NjY0NDczOQ.G-49o3.kFJ1db0cRXzKSeNO5nwHLHGKSF8fybKrbMibSk";
-            public static readonly ulong GuildId = 1095663326555553875; // suda id servera
+            public static readonly string Token = ""; // suda token
+            public static readonly ulong GuildId = 0; // suda id servera
         }
     }
 }
